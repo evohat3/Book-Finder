5fbb4616e9ca05bc157c0daf535d71a354f70352
evohat3 <118646438+evohat3@users.noreply.github.com>
2023-06-27 20:36:31 -0500

    update config variables

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,15 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks' || 'mongodb://127.0.0.1:27017/BookFinder',{
+    dbName: process.env.MONGODB_DB_NAME || 'googlebooks',
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+.then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((error) => {
+    console.error('Error connecting to MongoDB:', error);
+  });
 
 module.exports = mongoose.connection;
